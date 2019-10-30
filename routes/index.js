8722db12454dc20175b0317cead615d7feb56a12
William <zingon@163.com>
2019-10-30 15:48:02 +0800

    v07

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('BOE VR', {title: 'Express'});
+    res.render('index', {title: 'BOE VR'});
 });
 
 module.exports = router;
