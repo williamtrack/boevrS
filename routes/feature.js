07e75a13ea753db731e28c2a6fe3e0f855d5121c
William <zingon@163.com>
2020-05-27 11:32:10 +0800

    add rtmp web background

--- routes/feature.js
+++ routes/feature.js
@@ -19,6 +19,6 @@ router.get('/vrHouse', function (req, res) {
 });
 
 router.get('/rtmp', function (req, res) {
-    if (req.url === '/rtmp') return res.redirect(301, 'https://boevr.cn/static/rtmp.html');
-    // if (req.url === '/rtmp') return res.redirect(301, 'http://127.0.0.1/static/rtmp.html');
+    if (req.url === '/rtmp') return res.redirect(301, 'https://boevr.cn/static/rtmp/index.html');
+    // if (req.url === '/rtmp') return res.redirect(301, 'http://127.0.0.1/static/rtmp/index.html');
 });
